0fe6f591a7e38a8ddc8252f8c9418e895f2d367d
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-03-18 12:17:20 -0400

    more dropdown stuff

--- main.js
+++ main.js
@@ -77,28 +77,28 @@ locationDropdown.addEventListener('change', function() {
 
 
 
-  var bossList = 'https://us.api.battle.net/wow/boss/?locale=en_US&apikey='+ key;
-  $.ajax({
-    url: bossList
-  }).done(function(bossData){
-    console.log(bossData);
-    console.log(bossData.bosses[0].description);
-
-    for (var i = 0; i < bossData.bosses.length; i++) {
-
-      var name = bossData.bosses[i].name;
-      var optionEl = document.createElement('option');
-      optionEl.innerHTML = name;
-      optionEl.value = bossData.bosses[i].id;
-      bossDropdown.appendChild(optionEl);
-    }
-  }).fail(function(bossData){
-    console.log(bossData);
-  }).always(function(bossData){
-  })//ajax one
-
+  // var bossList = 'https://us.api.battle.net/wow/boss/?locale=en_US&apikey='+ key;
+  // $.ajax({
+  //   url: bossList
+  // }).done(function(bossData){
+  //   console.log(bossData);
+  //   console.log(bossData.bosses[0].description);
+  //
+  //   for (var i = 0; i < bossData.bosses.length; i++) {
+  //
+  //     var name = bossData.bosses[i].name;
+  //     var optionEl = document.createElement('option');
+  //     optionEl.innerHTML = name;
+  //     optionEl.value = bossData.bosses[i].id;
+  //     bossDropdown.appendChild(optionEl);
+  //   }
+  // }).fail(function(bossData){
+  //   console.log(bossData);
+  // }).always(function(bossData){
+  // })//ajax one
+  //
   var submit = document.querySelector('#submit')
-  submit.addEventListener('click', function(){
+  dropdown.addEventListener('change', function(){
 
 
   $.ajax({
@@ -110,17 +110,17 @@ locationDropdown.addEventListener('change', function() {
     // var zoneTag = document.querySelector('#zoneTag');
     var zone = bossData.zoneId;
 
-    $.ajax({
-      url: 'https://us.api.battle.net/wow/zone/'+ zone +'?locale=en_US&apikey=' + key
-    }).done(function(zoneData){
-      console.log('zone up');
-      console.log(zoneData);
-      var html = zoneTemplate(zoneData);
-      zoneDisplay.innerHTML = html;
-
-    })
-
-
+  //   $.ajax({
+  //     url: 'https://us.api.battle.net/wow/zone/'+ zone +'?locale=en_US&apikey=' + key
+  //   }).done(function(zoneData){
+  //     console.log('zone up');
+  //     console.log(zoneData);
+  //     var html = zoneTemplate(zoneData);
+  //     zoneDisplay.innerHTML = html;
+  //
+  //   })
+  //
+  //
   }).fail(function(bossData){
     console.log(bossData);
   })//ajax 2
